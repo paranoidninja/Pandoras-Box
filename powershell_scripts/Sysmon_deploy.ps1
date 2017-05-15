c1196151a34b0e3172907f30c3b125323777075f
paranoidninja <paranoidninja@protonmail.com>
2017-05-15 13:06:56 +0530

    new file:   powershell_scripts/Sysmon_deploy.ps1

---
+++ powershell_scripts/Sysmon_deploy.ps1
@@ -0,0 +1,19 @@
+##Author : Paranoid Ninja
+##Email  : paranoidninja@protonmail.com
+##Desc   : Simple Powershell Script to deploy any tool remotely via winrm. Change the SourceFolder and add all computer names in the CompName Variable text file/path
+
+$SourceFolder = "\\VBOXSVR\shared_box\psexec.exe\"
+$CompName = Get-Content "C:\Users\Administrator\Desktop\machine.txt"
+
+foreach ($computer in $CompName)
+{
+    $DestinationFolder = "\\$CompName\C$\sysmon"
+
+    if(!(Test-Path -path $DestinationFolder))
+    {
+        New-Item $DestinationFolder -Type Directory
+    }
+
+    robocopy $SourceFolder $DestinationFolder
+    Invoke-Command -ComputerName $computer -ScriptBlock { & cmd /c "C:\psexec.exe"} #use whatever tool you want
+}
