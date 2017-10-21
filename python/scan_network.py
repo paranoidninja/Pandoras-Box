60690548ceac56b5ea464cc4fbd19ceea529b086
paranoidninja <paranoidninja@protonmail.com>
2017-10-21 15:19:54 +0530

    modified:   python/scan_network.py

--- python/scan_network.py
+++ python/scan_network.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 
+#Change network interface if not working
+
 from scapy.all import arping
 import netifaces as ni
 
 print "[+] Scanning Wireless Network Interface...\n"
 
-if ni.AF_INET in ni.ifaddresses('wlp3s0'):
-	ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
-	ipsplit = ip.split('.')
-	ipsplit[3] = '*'
-	iprange = '.'.join(ipsplit)
-	arping(iprange)
+if 'wlp3s0' in ni.interfaces():
+	if ni.AF_INET in ni.ifaddresses('wlp3s0'):
+		ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
+		ipsplit = ip.split('.')
+		ipsplit[3] = '*'
+		iprange = '.'.join(ipsplit)
+		arping(iprange)
+	else:
+		print "[-] No IP has been assigned to Wireless Interface\n"
 else:
-	print "[-] No IP has been assigned to Wireless Interface\n"
+	print "[-] Interface wlp3s0 not found. Please change the interface name"
 
 print "[+] Scanning Ethernet Network Interface...\n"
 
-if ni.AF_INET in ni.ifaddresses('enp2s0'):
-	ip = ni.ifaddresses('enp2s0')[ni.AF_INET][0]['addr']
-	ipsplit = ip.split('.')
-	ipsplit[3] = '*'
-	iprange = '.'.join(ipsplit)
-	arping(iprange)
+if 'enp2s0' in ni.interfaces():
+	if ni.AF_INET in ni.ifaddresses('enp2s0'):
+		ip = ni.ifaddresses('enp2s0')[ni.AF_INET][0]['addr']
+		ipsplit = ip.split('.')
+		ipsplit[3] = '*'
+		iprange = '.'.join(ipsplit)
+		arping(iprange)
+	else:
+		print "[-] No IP has been assigned to Ethernet Interface"
 else:
-	print "[-] No IP has been assigned to Ethernet Interface"
+	print "[-] Interface enp2s0 not found. Please change the interface name"
